e5617e76dca021aefc80baf7931cca0cfbd4f257
Aditya Sanjaya <adityasanjaya24@gmail.com>
2019-05-15 13:21:11 +0700

    Initial Commit

---
+++ ModelingEntity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModelingEntity
+//
+//  Created by Aditya Sanjaya on 15/05/19.
+//  Copyright © 2019 Aditya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
